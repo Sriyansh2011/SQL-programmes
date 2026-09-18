df12c52d805c8f7a1c39d311a387eaa9256a46e2
Sriyansh2011 <iamthegoat1016@gmail.com>
2026-09-18 13:12:22 +0300

    Add files via upload

---
+++ Activity2.sql
@@ -0,0 +1,16 @@
+create table if not exists Salesman (
+     Salesman_id text primary key,
+     name text,
+     city text,
+     comission real 
+);
+
+INSERT INTO salesman (Salesman_id,name,city,Comission) VALUES
+('5001','James Hoog','New York',0.15),
+('5002','Nail Knite','Paris',0.13),
+('5005','Pit Alex','London',0.11),
+('5006','Mc Lyon','Paris',0.14),
+('5007','Paul Adam','Rome',0.13),
+('5003','Lauson Hen','San Jose',0.12);
+
+SELECT * FROM Salesman;
